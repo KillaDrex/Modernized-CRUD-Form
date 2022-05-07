6a23f323d5244bd647df97e11656a725232d0f37
Andre Aquino <aquino_andre.agustin@yahoo.com>
2022-05-07 11:00:29 +0300

    Created root window

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+
+public class Main extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Root Window");
+		Button button = new Button("Click me!");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 300, 250);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
